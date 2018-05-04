c8baccb63816f454090e62895b2f550e3c933cac
SRIDHAR KARUPPUSAMY <sridharlovevirus@gmail.com>
2018-05-04 16:49:28 +0530

    update process bar
    update done!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NgProgress } from 'ngx-progressbar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+constructor(public progressService: NgProgress) {
+	
+}
+ngOnInit() {
+	 this.progressService.start();
+  }
 }
